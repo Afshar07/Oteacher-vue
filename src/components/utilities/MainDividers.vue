824d482fee57ac5718a96eb4e7d8d862838c3fc1
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 15:29:14 +0430

    Add dividers

---
+++ src/components/utilities/MainDividers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="divider-container">
+    <hr class="divider-1" />
+    <hr class="divider-2" />
+  </div>
+</template>
+<style scoped>
+hr {
+  border: 2px solid #87e9ff;
+}
+.divider-1 {
+  width: 100%;
+}
+.divider-2 {
+  width: 70%;
+}
+</style>
