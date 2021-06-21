c87e7c3a28aa89fcfc64b0173c9a453e29a1dc14
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 17:42:26 +0430

    Add needed images and create SessionReservation component

---
+++ src/components/animated/SessionReservation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-container d-flex">
+    <img :src="reserveIcon" alt="reservation icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      reserveIcon: require("../../assets/reservation.svg"),
+    };
+  },
+};
+</script>
