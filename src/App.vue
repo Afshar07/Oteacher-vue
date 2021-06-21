40622b20e0ec231311c1a476f150191098a3a64b
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 12:56:23 +0430

    Add fonts and set the font-family

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <the-header class="header-nav" :class="stickyNav"></the-header>
+  <oteacher-intro></oteacher-intro>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import OteacherIntro from "./components/OteacherIntro.vue";
 export default {
   created() {
     window.addEventListener("scroll", this.handleScroll);
@@ -12,7 +14,7 @@ export default {
     window.removeEventListener("scroll", this.handleScroll);
   },
   name: "App",
-  components: { TheHeader },
+  components: { TheHeader, OteacherIntro },
   data() {
     return {
       isSticky: false,
@@ -37,11 +39,27 @@ export default {
 
 <style>
 @import url(//db.onlinewebfonts.com/c/2789580ebf164367a902a50203debe13?family=B+Yekan+);
-* {
+@font-face {
+  font-family: iranyekan;
+  font-style: normal;
+  font-weight: normal;
+  src: url("./assets/fonts/IranYekan/eot/IRANYekanWebRegular.eot");
+  src: url("./assets/fonts/IranYekan/eot/IRANYekanWebRegular.eot?#iefix")
+      format("embedded-opentype"),
+    url("./assets/fonts/IranYekan/woff/IRANYekanWebRegular.woff") format("woff"),
+    url("./assets/fonts/IranYekan/woff2/IRANYekanWebRegular.woff2")
+      format("woff2"),
+    url("./assets/fonts/IranYekan/ttf/IRANYekanWebRegular.ttf")
+      format("truetype");
+}
+
+*,
+*::after,
+*::before {
   margin: 0;
   padding: 0;
   box-sizing: inherit;
-  font-family: "B Yekan+", sans-serif;
+  font-family: "iranyekan", sans-serif;
   font-weight: 100;
 }
 html {
@@ -49,13 +67,17 @@ html {
 }
 body {
   box-sizing: border-box;
-  height: 200vh;
+  height: 100%;
 }
 .header-nav {
-  position: fixed;
-  top: 0;
+  /* position: sticky; */
+  /* top: 5px;   */
+  margin-bottom: 10rem;
+  background-color: #fff;
 }
 .nav-sticky {
+  position: sticky;
+  top: 0;
   box-shadow: 0 0.1rem 0.8rem rgb(95, 175, 238, 0.1);
 }
 </style>
