d3dbaabd05375544186fd709f2ec41c25fa8bf59
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 16:59:25 +0430

    Add icons and create SessionCalendar component

---
+++ src/components/animated/SessionCalendar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="calendar-container d-flex flex-column">
+    <div class="calendar-title">
+      <h2>ساعت برگزاری کلاس من</h2>
+      <span class="badge badge-info">آزمایشی رایگان</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.calendar-container {
+  position: relative;
+}
+</style>
