23e64b3f17bc450f1a70194f6873e748bcfa83ee
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 14:48:46 +0430

    Create template and add style for flags rendering

---
+++ src/components/SupportedLangs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="country-container d-flex flex-column align-items-center" dir="rtl">
+    <img :src="flagImg" alt="" class="country-flag" />
+    <p class="country-name">{{ name }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["flag", "name"],
+  data() {
+    return {
+      flagImg: require(`../assets/flags/${this.flag}`),
+    };
+  },
+};
+</script>
+<style scoped>
+.country-container {
+  margin-left: 4rem;
+  width: 10rem;
+  border-radius: 10px;
+  transition: all 0.3s;
+}
+.country-container:hover {
+  box-shadow: 0px 2px 10px 2px rgb(95, 175, 238, 0.1);
+}
+.country-flag {
+  width: 4rem;
+  margin-top: 2rem;
+}
+.country-name {
+  margin-top: 1rem;
+}
+</style>
