b15bee9b32bc9ac6973bbdd1c57cf4e277a32abd
Afshar07 <mr.afshar0@gmail.com>
2021-06-21 15:30:16 +0430

    Add learningSteps component

---
+++ src/components/LearningSteps.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="learning-container d-flex">
+    <div class="left-section col-5">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam fuga nam,
+      consequuntur sit molestiae enim? Sapiente harum sit officiis distinctio!
+    </div>
+    <div class="right-section col-7">
+      <teachers-name :img="teacher1Pic" :name="teacher1Name"></teachers-name>
+    </div>
+  </div>
+</template>
+
+<script>
+import TeachersName from "./animated/TeachersName.vue";
+export default {
+  components: { TeachersName },
+  data() {
+    return {
+      teacher1Pic: require("../assets/teacher1.png"),
+      teacher1Name: "استاد نگین معتضدی",
+    };
+  },
+};
+</script>
+<style scoped>
+.learning-container {
+  margin: 4rem;
+}
+.right-section {
+  height: 100vh;
+  background: url("../assets/orbit.svg") no-repeat center right;
+}
+</style>
