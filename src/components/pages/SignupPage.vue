f4cdeb4fd6464e50ab7ef202d4ecc11a6a74673d
Afshar07 <mr.afshar0@gmail.com>
2021-06-22 18:01:32 +0430

    Create SignupPage component

--- src/components/pages/SignupPage.vue
+++ src/components/pages/SignupPage.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="register-container d-flex flex-column align-items-center">
-    <img :src="logoUrl" alt="Oteacher logo" class="logo" />
+  <div
+    class="main-container d-flex flex-column align-items-center m-0 p-0"
+    dir="rtl"
+  >
+    <img :src="logoUrl" alt="Oteacher logo" class="logo mt-5 mb-3" />
+    <div class="register-container d-flex flex-column align-items-center">
+      <div
+        class="title d-flex align-items-center justify-content-between w-100"
+      >
+        <h2>ثبت نام در اُتیچر</h2>
+        <img :src="closeIcon" alt="close icon" />
+      </div>
+      <input type="text" class="w-75" />
+    </div>
   </div>
 </template>
 
@@ -9,18 +21,26 @@ export default {
   data() {
     return {
       logoUrl: require("../../assets/logo-signup.png"),
+      closeIcon: require("../../assets/close.svg"),
     };
   },
 };
 </script>
 
 <style scoped>
-.register-container {
-  background: url("../../assets/register-background.png") no-repeat center;
+.main-container {
+  background: url("../../assets/register-background.png") no-repeat 50%;
   background-size: 50rem;
   height: 100vh;
 }
 .logo {
-    width: 15rem;
+  width: 15rem;
+}
+.register-container {
+  width: 30%;
+  background-color: #fff;
+}
+.title {
+  padding: 1rem;
 }
 </style>
