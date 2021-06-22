9063b72494d2e9198be8674025750b5bf5753672
Afshar07 <mr.afshar0@gmail.com>
2021-06-22 10:08:02 +0430

    Finish animations

--- src/components/LearningSteps.vue
+++ src/components/LearningSteps.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="learning-container d-flex">
-    <div class="left-section col-5">
+    <div class="left-section col-md-5 col-sm-12">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam fuga nam,
       consequuntur sit molestiae enim? Sapiente harum sit officiis distinctio!
     </div>
-    <div class="right-section col-7">
+    <div class="right-section col-md-7">
       <div class="teacher-name">
         <teachers-name :img="teacher1Pic" :name="teacher1Name"></teachers-name>
         <teachers-name :img="teacher2Pic" :name="teacher2Name"></teachers-name>
@@ -34,6 +34,7 @@ export default {
       teacher2Pic: require("../assets/teacher2.png"),
       teacher1Name: "استاد نگین معتضدی",
       teacher2Name: "استاد محمد عمرانی",
+      removeBlur: false,
     };
   },
 };
@@ -56,20 +57,25 @@ export default {
   background-color: #fff;
   border-radius: 20px;
   box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+  animation: left-right 6s ease infinite, removeBlur 18s ease 3s infinite;
 }
 .teacher-rated {
   position: absolute;
   top: 8rem;
   right: 0;
+  animation: up-down 8s ease infinite, removeBlur 10s ease 5s infinite;
 }
 .session-reservation {
   position: absolute;
   top: 31rem;
   right: -1rem;
+  animation: right-left 6s ease infinite, removeBlur 13s ease infinite;
 }
 .session-calendar {
   position: absolute;
   top: 43rem;
+  left: 4rem;
+  animation: left-right 6s ease infinite, removeBlur 15s ease 7s infinite;
 }
 
 .teacher-name,
@@ -84,5 +90,58 @@ export default {
 .session-calendar:hover,
 .session-reservation:hover {
   filter: blur(0);
+}
+@keyframes up-down {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-3rem);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+@keyframes left-right {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(-3rem);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+@keyframes right-left {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(5rem);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+@keyframes removeBlur {
+  0% {
+    filter: blur(0.25rem);
+  }
+  20% {
+    filter: blur(0);
+  }
+  40% {
+    filter: blur(0);
+  }
+  60% {
+    filter: blur(0.25rem);
+  }
+  80% {
+    filter: blur(0.25rem);
+  }
+  100% {
+    filter: blur(0.25rem);
+  }
 }
 </style>
