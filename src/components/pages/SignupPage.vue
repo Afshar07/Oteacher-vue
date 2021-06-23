f278fdf1502f13631e79fb1c4151c979dc414bc0
Afshar07 <mr.afshar0@gmail.com>
2021-06-23 10:52:17 +0430

    Add signup btn and style it

--- src/components/pages/SignupPage.vue
+++ src/components/pages/SignupPage.vue
@@ -17,14 +17,22 @@
         />
       </div>
       <phone-input class="w-75"></phone-input>
+      <div class="checkbox-container d-flex align-items-center my-4">
+        <input type="checkbox" name="checkbox" class="ms-3" />
+        <label for="checkbox">
+          شرایط و قوانین استفاده از سایت اُتیچر را مطالعه کرده و می‌پذیرم
+        </label>
+      </div>
+      <main-button class="signup-btn mb-5">ثبت نام</main-button>
     </div>
   </div>
 </template>
 
 <script>
 import PhoneInput from "../utilities/PhoneInput.vue";
+import MainButton from "../utilities/MainButton.vue";
 export default {
-  components: { PhoneInput },
+  components: { PhoneInput, MainButton },
   data() {
     return {
       logoUrl: require("../../assets/logo-signup.png"),
@@ -57,5 +65,14 @@ export default {
 }
 .close-icon {
   cursor: pointer;
+}
+.signup-btn {
+  background-color: #3f9eff;
+  width: 60%;
+  color: #fff;
+}
+.signup-btn:hover {
+  background-color: #0f87ff;
+  transition: all 0.3s;
 }
 </style>
